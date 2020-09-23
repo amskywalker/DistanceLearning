f2ea795b65733c7b4b96416d4498569f4114bc22
Adailton Moura <adailton.silva091@academico.ifs.edu.br>
2020-09-22 22:54:31 -0300

    Creates model relationship.

--- app/Discipline.php
+++ app/Discipline.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Discipline extends Model
 {
-    //
+    public function remote_classes()
+    {
+        return $this->hasMany(RemoteClass::class, 'disciplines_id', 'id');
+    }
 }
--- app/RemoteClass.php
+++ app/RemoteClass.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class RemoteClass extends Model
 {
-    //
+    
+    public function disciplines()
+    {  
+        return $this->belongsTo(Discipline::class,'disciplines_id','id');  
+    }
 }
