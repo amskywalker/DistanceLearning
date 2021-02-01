be5054f9020734190ce8282e90de989d2ac0411e
adailton <adailtonmouradasilvasantos@gmail.com>
2021-02-01 04:02:09 -0300

    Criando uma sidebar e sidebar links

---
+++ resources/js/Components/Sidebar/Links/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    <inertia-link :href="href">
+        <div class="h-10 w-full my-1 hover:bg-gray-200 flex items-center">
+            <slot></slot>
+        </div>
+    </inertia-link>
+</template>
+<script>
+export default {
+    props: ['href'],
+}
+</script>
